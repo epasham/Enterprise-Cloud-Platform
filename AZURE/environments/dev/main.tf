af980a90951bd40a4a4e0152fcee470502f5d061
Ekambaram Pasham <ekambaram.pasham@gmail.com>
2025-07-25 12:34:11 +0530

    Update main.tf
    Signed-off-by: Ekambaram Pasham <ekambaram.pasham@gmail.com>

--- AZURE/environments/dev/main.tf
+++ AZURE/environments/dev/main.tf
@@ -1,1 +1,7 @@
-
+module "resource-group" {
+  source                  = "../../modules/resource-group"
+  count                   = var.create_resource_group ? 1 : 0
+  resource_group_name     = format("%s-%s-aks-rg", var.name, var.environment)
+  resource_group_location = var.resource_group_location
+  tags                    = var.tags
+}
