3d2d643a63b541f1a4f80cbcea41a535a5661fec
Ekambaram Pasham <ekambaram.pasham@gmail.com>
2025-07-25 14:44:41 +0530

    added acr module
    Signed-off-by: Ekambaram Pasham <ekambaram.pasham@gmail.com>

--- AZURE/environments/dev/main.tf
+++ AZURE/environments/dev/main.tf
@@ -1,19 +1,32 @@
+locals {
+  region         = "eastus"
+  environment    = "dev"
+  admin_enabled = true
+  sku = "Standard"
+
+  tags = {
+    Owner      = "infy"
+    Env        = "dev"
+    Department = "IT"
+  }
+}
+
 module "resource_group" {
   source                  = "../../modules/resource_group"
   count                   = var.create_resource_group ? 1 : 0
-  resource_group_name     = format("%s-%s-rg", var.name, var.environment)
-  resource_group_location = var.resource_group_location
-  tags                    = var.tags
+  resource_group_name     = format("%s-%s-rg", var.name, local.environment)
+  resource_group_location = local.location
+  tags                    = local.tags
 }
 
 module "acr" {
   source = "../../modules/acr"
   count      = var.acr_enabled ? 1 : 0
 
-  name = format("%s-%s-acr", var.name, var.environment)
-  environment = var.environment
+  name = format("%s-%s-acr", var.name, local.environment)
+  environment = local.environment
   resource_group = azurerm_resource_group.rg.name
-  location = var.resource_group_location
-  sku = var.acr_sku
-  tags = var.tags
+  location = local.location
+  sku = local.sku
+  tags = local.tags
 }
