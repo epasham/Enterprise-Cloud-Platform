98669819c64422785f36871bafe7231b134279f5
Ekambaram Pasham <ekambaram.pasham@gmail.com>
2025-07-25 14:15:21 +0530

    Update main.tf
    Signed-off-by: Ekambaram Pasham <ekambaram.pasham@gmail.com>

--- AZURE/environments/dev/main.tf
+++ AZURE/environments/dev/main.tf
@@ -5,3 +5,15 @@ module "resource-group" {
   resource_group_location = var.resource_group_location
   tags                    = var.tags
 }
+
+module "acr" {
+  source = "../../modules/acr"
+  count      = var.acr_enabled ? 1 : 0
+
+  name = format("%s-%s-acr", var.name, var.environment)
+  environment = var.environment
+  resource_group = azurerm_resource_group.rg.name
+  location = var.resource_group_location
+  sku = var.acr_sku
+  tags = var.tags
+}
