ff5ebddd0a649b042629e46f374cd214acb1491b
Ekambaram Pasham <ekambaram.pasham@gmail.com>
2025-07-25 14:39:14 +0530

    Update main.tf
    Signed-off-by: Ekambaram Pasham <ekambaram.pasham@gmail.com>

--- AZURE/modules/acr/main.tf
+++ AZURE/modules/acr/main.tf
@@ -2,11 +2,10 @@ resource "azurerm_container_registry" "acr" {
   name                = var.name
   resource_group_name = var.resource_group
   location            = var.location
-  admin_enabled       = true
+  admin_enabled       = var.admin_enabled
   sku                 = var.sku
 
   tags {
-    org   = "infy"
     env   = var.environment
   }
 }
