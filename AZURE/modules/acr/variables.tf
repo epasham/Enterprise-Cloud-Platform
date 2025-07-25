a13e09d3072a6002b9a36dca8802a60f657f1400
Ekambaram Pasham <ekambaram.pasham@gmail.com>
2025-07-25 14:38:09 +0530

    Update variables.tf
    Signed-off-by: Ekambaram Pasham <ekambaram.pasham@gmail.com>

--- AZURE/modules/acr/variables.tf
+++ AZURE/modules/acr/variables.tf
@@ -1,8 +1,22 @@
-variable "name" {}
 variable "environment" {}
 variable "resource_group" {}
-variable "location" {}
+
+variable "name" {
+  description = "The name of the ACR"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region of the ACR"
+  type        = string
+}
 
 variable "sku" {
-  default = "Standard"
+  description = "The SKU of the ACR"
+  type        = string
+}
+
+variable "admin_enabled" {
+  description = "Admin access enabled"
+  type        = bool
 }
