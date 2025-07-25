8b625e17a49c7a802f4e6fa015d9a8b4d2a56aff
Ekambaram Pasham <ekambaram.pasham@gmail.com>
2025-07-25 12:37:53 +0530

    Update versions.tf
    Signed-off-by: Ekambaram Pasham <ekambaram.pasham@gmail.com>

--- AZURE/environments/dev/versions.tf
+++ AZURE/environments/dev/versions.tf
@@ -4,13 +4,5 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">=2.13.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">=2.6"
-    }
   }
 }
